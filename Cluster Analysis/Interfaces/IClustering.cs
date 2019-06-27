8facf3e9cc0743f465c89556475c569f960d0558
kondrashov-ma <kondrashov-ma@WS-KONDRASHOV>
2019-06-27 09:42:49 +0300

    Update

--- Cluster Analysis/Interfaces/IClustering.cs
+++ Cluster Analysis/Interfaces/IClustering.cs
@@ -1,9 +1,6 @@
 ﻿using Cluster_Analysis.CommonClasses;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Cluster_Analysis
 {
